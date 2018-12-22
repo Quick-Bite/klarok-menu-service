39766677c4584a5b82f620fabc47657dc948bf04
DymaxionMap <DymaxionMap@users.noreply.github.com>
2018-12-21 17:04:26 -0800

    Implement GET route handler for serving all menu items for a given restaurant

--- server/index.js
+++ server/index.js
@@ -9,5 +9,18 @@ app.use(express.static('public'));
 
 app.get('/restaurants/:id', (req, res) => res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html')));
 
+app.get('/restaurants/:id/menu-items', (req, res) => {
+  const restaurantId = req.params.id;
+  MenuItem.find({ restaurantId })
+    .then((menuItems) => {
+      console.log(menuItems[0]);
+      res.send(menuItems);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(404);
+    });
+});
+
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Listening on localhost:${PORT}`));
