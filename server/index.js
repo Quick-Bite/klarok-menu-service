94df9a39345bec02970d48dcb4750e7af9435564
DymaxionMap <DymaxionMap@users.noreply.github.com>
2018-12-21 16:39:56 -0800

    Define GET route to serve index.html for a specific restaurant rather than the landing page

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const MenuItem = require('../database');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'public', 'index.html')));
+app.get('/restaurants/:id', (req, res) => res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html')));
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Listening on localhost:${PORT}`));
