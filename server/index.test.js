7dbf57107f4d9585e26e36027a525b64b7572cab
DymaxionMap <DymaxionMap@users.noreply.github.com>
2018-12-22 15:45:54 -0800

    Add unit test for fetching all menu items
    -Also rename unit test for fetching one menu item to be more
    understandable

--- server/index.test.js
+++ server/index.test.js
@@ -3,7 +3,7 @@
  */
 const axios = require('axios');
 
-test('GET request /restaurants/1/menu-items/0 fetchs item details', async () => {
+test('Fetch data for one menu item', async () => {
   expect.assertions(2);
   const response = await axios.get('http://localhost:3000/restaurants/1/menu-items/0');
   const menuItem = response.data;
@@ -13,4 +13,18 @@ test('GET request /restaurants/1/menu-items/0 fetchs item details', async () => 
     itemId: 0,
     name: 'Baby concept',
   });
+});
+
+test('Fetch summary data for all menu items', async () => {
+  expect.assertions(4);
+  const response = await axios.get('http://localhost:3000/restaurants/1/menu-items/');
+  const menuItems = response.data;
+  expect(Array.isArray(menuItems)).toBe(true);
+  expect(menuItems.length).toBe(19);
+  expect(menuItems[0]).toMatchObject({
+    restaurantId: 1,
+    itemId: 0,
+    name: 'Baby concept',
+  });
+  expect(menuItems.requiredChoiceCategories).toBe(undefined);
 });
