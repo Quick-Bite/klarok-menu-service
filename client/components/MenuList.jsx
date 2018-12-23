87887764551a087b5e6a4df943d3de2a2af0288f
DymaxionMap <DymaxionMap@users.noreply.github.com>
2018-12-23 11:24:21 -0800

    Factor out menu list item into its own component

--- client/components/MenuList.jsx
+++ client/components/MenuList.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import MenuListItem from './MenuListItem';
 
 const MenuList = ({ items, categories }) => (
   <div>
     {categories.map(category => (
       <div key={category}>
-        <h1>{category}</h1>
+        <h2>{category}</h2>
         <ul>
           {items
             .filter(item => item.category === category)
-            .map(item => <li key={item.itemId}>{item.name}</li>)}
+            .map(item => (
+              <li key={item.itemId}>
+                <MenuListItem item={item} />
+              </li>
+            ))}
         </ul>
       </div>
     ))}
---
+++ client/components/MenuListItem.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MenuListItem = ({ item }) => (
+  <div>
+    <h3>{item.name}</h3>
+    <p>{item.price}</p>
+    <p>{item.description}</p>
+    {item.popular ? <p>Popular</p> : null}
+    {item.spicy ? <p>Spicy</p> : null}
+    <img src={item.pictureUrl} alt="menu item" />
+  </div>
+);
+
+MenuListItem.propTypes = {
+  item: PropTypes.shape({
+    itemId: PropTypes.number.isRequired,
+    restaurantId: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    pictureUrl: PropTypes.string.isRequired,
+    popular: PropTypes.bool.isRequired,
+    spicy: PropTypes.bool.isRequired,
+  }).isRequired,
+};
+
+export default MenuListItem;
