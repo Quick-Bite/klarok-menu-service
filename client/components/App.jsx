afa1f0ae081371a2443a583704a07bef93c869be
DymaxionMap <DymaxionMap@users.noreply.github.com>
2018-12-22 16:53:54 -0800

    Refactor App into a class component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import MenuList from './MenuList';
 
-const App = () => (
-  <MenuList items={[{ name: 'Item1' }, { name: 'Item2' }, { name: 'Item3' }]} />
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [{ name: 'Item1' }, { name: 'Item2' }, { name: 'Item3' }] };
+  }
+
+  render() {
+    const { items } = this.state;
+    return (
+      <MenuList items={items} />
+    );
+  }
+}
 
 export default App;
