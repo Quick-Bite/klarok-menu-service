671e81498cc21a44d3d054bc58ed21d85bd950ad
DymaxionMap <DymaxionMap@users.noreply.github.com>
2018-12-22 17:27:40 -0800

    Fetch menu items from API

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 import MenuList from './MenuList';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { items: [{ name: 'Item1' }, { name: 'Item2' }, { name: 'Item3' }] };
+    this.state = { items: [] };
+  }
+
+  componentDidMount() {
+    const url = `${window.location.pathname}/menu-items`;
+    axios.get(url.replace('//', '/'))
+      .then((response) => {
+        const items = response.data;
+        this.setState({ items });
+      });
   }
 
   render() {
