7f817faccfe436c6ff197fa7729cfe5f212291fa
DymaxionMap <DymaxionMap@users.noreply.github.com>
2018-12-23 21:18:21 -0800

    Track quantity state in QuantityPicker

--- client/components/AddItem/QuantityPicker.jsx
+++ client/components/AddItem/QuantityPicker.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 
-const QuantityPicker = () => (
-  <div>
-    <h5>Quantity</h5>
-    <div>
-      <button type="button">-</button>
-      <input type="number" />
-      <button type="button">+</button>
-    </div>
-  </div>
-);
+class QuantityPicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { quantity: 0 };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const inputElement = event.target;
+    const quantity = Number(inputElement.value);
+    inputElement.value = quantity;
+    this.setState({ quantity });
+  }
+
+  render() {
+    const { quantity } = this.state;
+    return (
+      <div>
+        <h5>Quantity</h5>
+        <div>
+          <button type="button">-</button>
+          <input value={quantity} type="number" onChange={this.handleChange} />
+          <button type="button">+</button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default QuantityPicker;
