7fa877a28a116712224d60a800e5c8b378bd4f35
DymaxionMap <DymaxionMap@users.noreply.github.com>
2018-12-23 18:33:50 -0800

    Move Enzyme configuration step into its own file

--- client/components/MenuList.test.jsx
+++ client/components/MenuList.test.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import MenuList from './MenuList';
 import MenuListItem from './MenuListItem';
-
-configure({ adapter: new Adapter() });
 
 describe('MenuList', () => {
   let items;
