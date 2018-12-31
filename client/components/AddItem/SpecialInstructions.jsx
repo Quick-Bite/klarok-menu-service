f8998e6b8a97e1625555cf89c55a713097faaed8
DymaxionMap <DymaxionMap@users.noreply.github.com>
2018-12-30 16:34:54 -0800

    Style Special Instructions

--- client/components/AddItem/SpecialInstructions.jsx
+++ client/components/AddItem/SpecialInstructions.jsx
@@ -1,16 +1,42 @@
-/* eslint react/self-closing-comp: 0 */
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  margin-top: 10px;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  box-sizing: border-box;
+`;
+
+const Header = styled.h5`
+  margin: 0;
+  margin-bottom: 4px;
+  font-size: 16.5px;
+`;
+
+const TextArea = styled.textarea`
+  height: 76px;
+  width: 100%;
+  padding: 16px;
+  border: 2px solid #CACACA;
+  border-radius: 4px;
+  box-sizing: border-box;
+  font-size: 15px;
+  font-family: 'Nunito Sans', sans-serif;
+`;
 
 const SpecialInstructions = ({ handleChange }) => (
-  <div>
-    <h5>Special instructions</h5>
-    <textarea
+  <Wrapper>
+    <Header>Special instructions</Header>
+    <TextArea
       placeholder="Dressing on the side? No pickles? Let us know here."
       onChange={handleChange}
     >
-    </textarea>
-  </div>
+    </TextArea>
+  </Wrapper>
 );
 
 SpecialInstructions.propTypes = {
