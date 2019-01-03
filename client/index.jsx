d08ddcb727a49e55fcebce86523b82adde2793b2
DymaxionMap <DymaxionMap@users.noreply.github.com>
2019-01-03 11:07:39 -0800

    Rename App to Menu and render on div with id "Menu"

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import Menu from './components/Menu';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Menu />, document.getElementById('Menu'));
