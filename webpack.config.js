57d42c67ac0a1fa1f7ff54641469d5e244e12bc2
DymaxionMap <DymaxionMap@users.noreply.github.com>
2019-01-05 17:25:35 -0800

    Switch webpack to production

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, 'client', 'index.jsx'),
-  mode: 'development',
+  mode: 'production',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
